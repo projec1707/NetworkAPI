51fadc7fa2f9e9c4f4883e0e2bb432fbad260b95
projec1707 <pwroma@gmail.com>
2023-05-09 12:31:30 +0300

    Initial Commit

---
+++ NetworkAPI/NetworkAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkAPIApp.swift
+//  NetworkAPI
+//
+//  Created by Roman Riepa on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
